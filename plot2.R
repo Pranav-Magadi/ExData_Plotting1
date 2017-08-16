d0a61a976002ea94b537d1eb228a696757683248
Pranav-Magadi <pranav.magadi97@gmail.com>
2017-08-16 23:38:40 +0530

    Plots and code

---
+++ plot2.R
@@ -0,0 +1,9 @@
+## Working directory should be set to the folder containing household_power_consumption.txt
+
+## The values for skip and nrows in read.table were calculated interactively using the grep and readLines function.
+
+hpcdata<-read.table("household_power_consumption.txt",sep=";" ,skip = 66637,nrows = 2881, stringsAsFactors = FALSE,na.strings = "?",col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+datetime<-strptime(paste(hpcdata$Date,hpcdata$Time,sep=" "),"%d/%m/%Y %H:%M:%S")
+png(filename = "plot2.png",width=480,height=480,units = "px")
+plot(datetime,hpcdata$Global_active_power,type="l",xlab="",ylab = "Global Active Power (kilowatts)")
+dev.off()
